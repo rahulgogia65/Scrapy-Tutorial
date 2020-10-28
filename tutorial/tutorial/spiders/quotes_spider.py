e1911a6b6d8ff7a2e679883dc865d18a4a486f3a
rahulgogia65 <55249296+rahulgogia65@users.noreply.github.com>
2020-10-29 00:48:48 +0530

    Storing the scraped data
    Extracting and storing the scraped data

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,19 +1,17 @@
-import scrapy 
+import scrapy
+
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-
-   
-     start_urls = [
+    start_urls = [
         'http://quotes.toscrape.com/page/1/',
         'http://quotes.toscrape.com/page/2/',
     ]
 
-        
-    
-    def parse(self,response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-            self.log(f'saved file {filename}')
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
