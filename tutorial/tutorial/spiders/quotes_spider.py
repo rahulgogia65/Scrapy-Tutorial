66d76a45d27e100b25e5a647f8b23e64853a9f0b
rahulgogia65 <55249296+rahulgogia65@users.noreply.github.com>
2020-10-30 21:04:46 +0530

    Following Links Part 1
    A shortcut for creating Requests

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -4,8 +4,8 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
+        'https://quotes.toscrape.com/',
+        
     ]
 
     def parse(self, response):
@@ -15,3 +15,5 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall(),
             }
+
+        yield from response.follow_all(css='ul.pager a', callback=self.parse)
